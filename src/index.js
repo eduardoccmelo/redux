bcf82a6569a8af0152039eef80ae03099559faf5
Eduardo Melo <eduardoccmelo@gmail.com>
2021-07-01 16:20:53 +0200

    array methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import { compose, pipe } from "lodash/fp";
+// import { compose, pipe } from "lodash/fp";
 
-let input = "   JavaScript   ";
-let output = "<div>" + input.trim() + "</div>";
+const { add } = require("lodash");
 
-const trim = (str) => str.trim();
-const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
-const toLowerCase = (str) => str.toLowerCase();
+// let input = "   JavaScript   ";
+// let output = "<div>" + input.trim() + "</div>";
+
+// const trim = (str) => str.trim();
+// const wrap = (type) => (str) => `<${type}>${str}</${type}>`;
+// const toLowerCase = (str) => str.toLowerCase();
 
 // const result = wrapInDiv(toLowerCase(trim(input)));
-const transform = pipe(trim, toLowerCase, wrap("span"));
-console.log(transform(input));
+// const transform = pipe(trim, toLowerCase, wrap("span"));
+// console.log(transform(input));
+
+// const person = { name: "John" };
+// const updated = { ...person, name: "Bob" };
+// console.log(updated);
+
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+//Adding
+const index = numbers.indexOf(2);
+const added = [...numbers.slice(0, index), 4, ...numbers.slice(index)];
+console.log(added);
+
+//Removing
+const removed = numbers.filter((num) => num !== 2);
+
+//Updating
+const updated = numbers.map((num) => (num === 2 ? 20 : num));
+console.log(updated);
